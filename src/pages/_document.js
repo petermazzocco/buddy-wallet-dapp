2dc396e540d981bfb547b48577f1c03feabedaf1
PM <100664895+OhItsPM@users.noreply.github.com>
2023-05-20 17:05:45 -0700

    testing html tag changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>BUDDY WALLET</title>
-        <meta
-          content="Buddy Wallet - View & Transact With Your NFT's Buddy Wallet Using ERC6551"
-          name="description"
-        />
-        <link href="/favicon.ico" rel="shortcut icon" />
       </Head>
       <body>
         <Main />
