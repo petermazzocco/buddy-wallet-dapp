9a777e7dc966845f59bd1b34bcb9ede8e54400e1
PM <100664895+OhItsPM@users.noreply.github.com>
2023-05-22 20:56:47 -0700

    removed viem

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,71 +17,71 @@ export default function Main() {
   const { address, isConnected } = useAccount(); // Wagmi account details
   const [copied, setCopied] = useState(false); // Copied state for address
 
-  /**
-   * Create the provider client for Tokenbound SDK via Viem
-   * @returns providerClient
-   */
-  const providerClient = createPublicClient({
-    chain: goerli || mainnet,
-    transport: custom(window.ethereum),
-  });
-
   // /**
-  //  * Alchemy SDK configuration
-  //  * @returns alchemy client
+  //  * Create the provider client for Tokenbound SDK via Viem
+  //  * @returns providerClient
   //  */
-  // const ALCHEMY_API = process.env.NEXT_PUBLIC_ALCHEMY_API;
-  // const config = {
-  //   apiKey: ALCHEMY_API,
-  //   network: Network.ETH_MAINNET,
-  // };
-  // const alchemy = new Alchemy(config);
-
-  // /**
-  //  * Get the NFT data for the connected address via Alchemy
-  //  * @returns nfts
-  //  */
-  // useEffect(() => {
-  //   const main = async () => {
-  //     async function getNftsForOwner() {
-  //       if (isConnected) {
-  //         try {
-  //           let nftArray = [];
-  //           const nftsIterable = alchemy.nft.getNftsForOwnerIterator(address);
-  //           for await (const nft of nftsIterable) {
-  //             nftArray.push(nft);
-  //           }
-  //           setNfts(nftArray);
-  //         } catch (err) {
-  //           setErrorMsg("An error occurred while fetching NFTs");
-  //           console.log(err?.message);
-  //         }
-  //       }
-  //     }
-  //     getNftsForOwner();
-  //   };
-  //   main();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [address]);
+  // const providerClient = createPublicClient({
+  //   chain: goerli || mainnet,
+  //   transport: custom(window.ethereum),
+  // });
 
   /**
-   * Get the ERC6551 address (or Buddy Wallet) from all available ERC721 addresses
-   * @param contractAdd
-   * @param tokenId
+   * Alchemy SDK configuration
+   * @returns alchemy client
    */
-
-  const handleAddress = async (contractAdd, tokenId) => {
-    try {
-      const accountAddress = await getAccount(
-        contractAdd,
-        tokenId,
-        providerClient
-      );
-      setBuddy(accountAddress);
-    } catch (err) {
-      setErrorMsg("An error occured while getting the address");
-    }
+  const ALCHEMY_API = process.env.NEXT_PUBLIC_ALCHEMY_API;
+  const config = {
+    apiKey: ALCHEMY_API,
+    network: Network.ETH_MAINNET,
   };
+  const alchemy = new Alchemy(config);
+
+  /**
+   * Get the NFT data for the connected address via Alchemy
+   * @returns nfts
+   */
+  useEffect(() => {
+    const main = async () => {
+      async function getNftsForOwner() {
+        if (isConnected) {
+          try {
+            let nftArray = [];
+            const nftsIterable = alchemy.nft.getNftsForOwnerIterator(address);
+            for await (const nft of nftsIterable) {
+              nftArray.push(nft);
+            }
+            setNfts(nftArray);
+          } catch (err) {
+            setErrorMsg("An error occurred while fetching NFTs");
+            console.log(err?.message);
+          }
+        }
+      }
+      getNftsForOwner();
+    };
+    main();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [address]);
+
+  // /**
+  //  * Get the ERC6551 address (or Buddy Wallet) from all available ERC721 addresses
+  //  * @param contractAdd
+  //  * @param tokenId
+  //  */
+
+  // const handleAddress = async (contractAdd, tokenId) => {
+  //   try {
+  //     const accountAddress = await getAccount(
+  //       contractAdd,
+  //       tokenId,
+  //       providerClient
+  //     );
+  //     setBuddy(accountAddress);
+  //   } catch (err) {
+  //     setErrorMsg("An error occured while getting the address");
+  //   }
+  // };
 
   /**
    * Logic to for modal component
